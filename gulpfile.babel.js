7f5a037560410d87363aab518034cf0250dad244
Marvin Roger <nivramdu94@gmail.com>
2015-10-07 18:48:58 +0200

    Optimize gulpfile by using newer and ES6

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,29 +1,27 @@
-var path = require('path');
-
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
+var gulp = require('gulp-help')(require('gulp')); // adds a default help task
+var plumber = require('gulp-plumber'); // help to avoid crash if error in a task
 var watch = require('gulp-watch'); // gulp.watch doesn't detect new files
-var notifier = require('node-notifier');
-require('gulp-help')(gulp);
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
-var del = require('del');
-var imagemin = require('gulp-imagemin');
-var koutoSwiss = require('kouto-swiss');
-var minifyCss = require('gulp-minify-css');
-var replace = require('gulp-replace');
-var runSequence = require('run-sequence');
-var sourcemaps = require('gulp-sourcemaps');
-var stylus = require('gulp-stylus');
-var uglify = require('gulp-uglify');
-var nodemon = require('gulp-nodemon-tempfix');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var rev = require('gulp-rev');
-var revReplace = require('gulp-rev-replace');
-var riotify = require('riotify');
+var notifier = require('node-notifier'); // desktop notifications
+var autoprefixer = require('gulp-autoprefixer'); // autoprefix css
+var browserSync = require('browser-sync').create(); // auto refresh browsers
+var del = require('del'); // delete files
+var imagemin = require('gulp-imagemin'); // optimize images
+var koutoSwiss = require('kouto-swiss'); // Stylus mixins
+var minifyCss = require('gulp-minify-css'); // minify css
+var replace = require('gulp-replace'); // replace text in files, useful for humans.txt update date
+var runSequence = require('run-sequence'); // run tasks in series or in parallel
+var sourcemaps = require('gulp-sourcemaps'); // generate source maps
+var stylus = require('gulp-stylus'); // compile stylus to CSS
+var uglify = require('gulp-uglify'); // minify JS
+var nodemon = require('gulp-nodemon-tempfix'); // reload server JS app on change / restart on crash
+var browserify = require('browserify'); // helps using modules client side
+var babelify = require('babelify'); // Babel transform for browserify
+var source = require('vinyl-source-stream'); // helper for browserify text stream to gulp pipeline
+var buffer = require('vinyl-buffer'); // helper for browserify
+var rev = require('gulp-rev'); // Cache bust
+var revReplace = require('gulp-rev-replace'); // Replace cache busted occurences in files
+var riotify = require('riotify'); // Riot transform for browserify
+var newer = require('gulp-newer'); // only process changed files in tasks
 
 //  ##################
 //  # Error notifier #
@@ -34,11 +32,11 @@ var errored = false;
 var errorHandler = function (task) {
   return function (error) {
     errored = true;
-    console.log('Error in ' + task + ': ' + error.message);
+    console.log(`Error in ${task}: ${error.message}`);
     notifier.notify({
-      title: 'Error in ' + task,
+      title: `Error in ${task}`,
       message: error.message,
-      icon: path.join(__dirname, 'error.png')
+      icon: require('path').join(__dirname, 'error.png')
     });
   };
 };
@@ -50,6 +48,7 @@ var errorHandler = function (task) {
 gulp.task('stylus', 'Process main.styl with sourcemap support', function () {
   return gulp.src('./app/css/**/[^_]*.styl')
     .pipe(plumber(errorHandler('stylus')))
+    .pipe(newer({ dest: './public/css', 'ext': '.css' }))
     .pipe(stylus({
       sourcemap: {
         inline: true
@@ -71,7 +70,7 @@ gulp.task('stylus', 'Process main.styl with sourcemap support', function () {
     .pipe(browserSync.stream({ match: '**/*.css' })); // match: to avoid full reload as browsersync won't find map file in the dom
 });
 
-gulp.task('es6-7', 'Process CoffeeScript files with sourcemap support', function () {
+gulp.task('es6-7', 'Process ES6/7 files with sourcemap support', function () {
   return browserify('./app/js/app.js')
     .transform(riotify, { type: 'es6' })
     .transform(babelify.configure({ optional: ['runtime'] }))
@@ -119,29 +118,29 @@ gulp.task('dev', 'Run stylus and es6-7 on file change with BrowserSync support',
   // Watch to build
 
   watch('./app/assets/**/*', function (vinyl) {
-    console.log(vinyl.path + ' was ' + vinyl.event + ', piping to public/...');
+    console.log(`${vinyl.path} was ${vinyl.event}, piping to public/...`);
     runSequence('assets');
   });
 
   watch('./app/vendor/**/*', function (vinyl) {
-    console.log(vinyl.path + ' was ' + vinyl.event + ', piping to public/vendor/...');
+    console.log(`${vinyl.path} was ${vinyl.event}, piping to public/vendor/...`);
     runSequence('vendor');
   });
 
   watch('./app/css/**/*.styl', function (vinyl) {
-    console.log(vinyl.path + ' was ' + vinyl.event + ', running Stylus...');
+    console.log(`${vinyl.path} was ${vinyl.event}, running Stylus...`);
     runSequence('stylus');
   });
 
   watch('./app/js/**/*.{js,tag}', function (vinyl) {
-    console.log(vinyl.path + ' was ' + vinyl.event + ', running ES6-7 to ES5...');
+    console.log(`${vinyl.path} was ${vinyl.event}, running ES6-7 to ES5...`);
     runSequence('es6-7');
   });
 
   // Watch to reload
 
   watch('./views/**/*.html', function (vinyl) {
-    console.log(vinyl.path + ' was ' + vinyl.event + ', reloading browsers...');
+    console.log(`${vinyl.path} was ${vinyl.event}, reloading browsers...`);
     browserSync.reload();
   });
 });
@@ -155,6 +154,7 @@ gulp.task('assets', 'Copy assets to public directory', function () {
     base: './app/assets'
   })
     .pipe(plumber(errorHandler('assets')))
+    .pipe(newer('./public'))
     .pipe(gulp.dest('./public'))
     .on('end', browserSync.reload);
 });
@@ -164,16 +164,17 @@ gulp.task('vendor', 'Copy vendor to public directory', function (done) {
     base: './app'
   })
     .pipe(plumber(errorHandler('vendor')))
+    .pipe(newer('./public'))
     .pipe(gulp.dest('./public'))
     .on('end', browserSync.reload);
 });
 
 gulp.task('humans', 'Update humans.txt update date', function () {
   var date = new Date();
-  var formattedDate = date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + ('0' + date.getDate()).slice(-2);
+  var formattedDate = `${date.getFullYear()}/${('0' + (date.getMonth() + 1)).slice(-2)}/${('0' + date.getDate()).slice(-2)}`;
   return gulp.src(['./public/humans.txt'])
     .pipe(plumber(errorHandler('humans')))
-    .pipe(replace('#last_update#', formattedDate))
+    .pipe(replace('${lastUpdate}', formattedDate))
     .pipe(gulp.dest('./public'));
 });
 
@@ -240,10 +241,10 @@ gulp.task('dist:cachebust-clean', 'Clean cache bust files in build', function (d
   });
 });
 
-gulp.task('dist', 'Dist project into a dist directory', ['build'], function (done) {
-  runSequence('dist:clean', 'dist:copy', 'dist:imagemin', 'dist:cachebust', 'dist:cachebust-replace', 'dist:cachebust-clean', function () {
+gulp.task('dist', 'Dist project into a dist directory', function (done) {
+  runSequence('build', 'dist:clean', 'dist:copy', 'dist:imagemin', 'dist:cachebust', 'dist:cachebust-replace', 'dist:cachebust-clean', function () {
     if (errored) {
-      console.log('The build failed, cleaning dist directory');
+      console.log('Build failed, cleaning dist directory');
       runSequence('dist:clean', function () {
         process.exit(-1);
       });
